1afcecc4c098cbe499706d7a19ed31a516dee456
Thapelo <chabathapelo1@gmail.com>
2026-05-27 19:59:03 +0200

    frontend:implentation

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
+import supabase from "../src/lib/supabase"
 import styles from "./page.module.css";
-
+import { useEffect,useState } from "react";
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
   srcDark: string;
@@ -19,53 +21,36 @@ const ThemeImage = (props: Props) => {
 };
 
 export default function Home() {
+
+const [fetchError, setFetchError] = useState<string | null>(null);
+const [tasks, setTasks] = useState<any[] | null>(null);
+  useEffect(()=>{
+    const fetchTasks = async () => {
+      const {data,error} = await supabase
+      .from("tasks")
+      .select("*")
+      if(error){
+        setFetchError("Could not fetch tasks");
+        setTasks(null)
+        console.log(error)
+      }
+      if(data){
+        setTasks(data);
+        setFetchError(null)
+        console.log("DATA:", data);
+        console.log("ERROR:", error);
+      }
+    }
+    fetchTasks();
+  },[])
+  
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ThemeImage
-          className={styles.logo}
-          srcLight="turborepo-dark.svg"
-          srcDark="turborepo-light.svg"
-          alt="Turborepo logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>apps/web/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new/clone?demo-description=Learn+to+implement+a+monorepo+with+a+two+Next.js+sites+that+has+installed+three+local+packages.&demo-image=%2F%2Fimages.ctfassets.net%2Fe5382hct74si%2F4K8ZISWAzJ8X1504ca0zmC%2F0b21a1c6246add355e55816278ef54bc%2FBasic.png&demo-title=Monorepo+with+Turborepo&demo-url=https%3A%2F%2Fexamples-basic-web.vercel.sh%2F&from=templates&project-name=Monorepo+with+Turborepo&repository-name=monorepo-turborepo&repository-url=https%3A%2F%2Fgithub.com%2Fvercel%2Fturborepo%2Ftree%2Fmain%2Fexamples%2Fbasic&root-directory=apps%2Fdocs&skippable-integrations=1&teamSlug=vercel&utm_source=create-turbo"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://turborepo.dev/docs?utm_source"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
+        {fetchError && <p>{fetchError}nbv</p>}
+        {tasks?.map((task:any) => (
+          <p key={task.id}>{task.title}</p>
+        ))}
       </main>
       <footer className={styles.footer}>
         <a
